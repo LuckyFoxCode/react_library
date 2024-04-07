39992758c4652d96aa59f151146f0617523a546a
Serhii Shabala <sergei181989@gmail.com>
2024-04-07 15:37:06 +0300

    :recycle: refactore Button component.

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
-export const Button = ({ title, className, onClick }) => {
+export const Button = ({ type, title, className, onClick }) => {
   return (
     <button
+      type={type}
       onClick={onClick}
       className={clsx(
         'flex items-center justify-center min-w-[75px] py-[9px] text-[10px] font-bold capitalize border border-dark rounded-sm hover:bg-gold transition',
@@ -19,4 +20,5 @@ Button.propTypes = {
   onClick: PropTypes.func,
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['button', 'submit']).isRequired,
 };
